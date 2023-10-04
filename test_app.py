047bf0a9f6bfc6752df442abed76a7f7eab836fd
Anish Rajwani <75964487+19ANISH@users.noreply.github.com>
2023-10-04 11:32:01 +0530

    Updated file /test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,3 @@
-import pytest
-import requests
-def test_get_endpoint():
-    response = requests.get('http://localhost:8000/')
-    #pass the link for requests.get() to get it tested	
-    assert response.status_code == 200
-    # You can also check the response payload or other attributes using assertions
-    # For example, assert response.json() == expected_data
-
+n = 5
+for i in range(n):
+    print(i)
